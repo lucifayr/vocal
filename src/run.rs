8e55322dac14555c148bed3470e33ac5d6c0f537
jackboxx <lucygschwantner@gmail.com>
2023-01-19 21:29:02 +0100

    use clamp instead of min and max

--- src/run.rs
+++ src/run.rs
@@ -1,7 +1,4 @@
-use std::{
-    cmp::{max, min},
-    io,
-};
+use std::io;
 
 use tui::{backend::CrosstermBackend, Terminal};
 
@@ -25,8 +22,8 @@ pub fn run(config: Config, args: Args) -> Result<(), &'static str> {
         Err(_) => return Err("Failed to create a TUI terminal"),
     };
 
-    let volume = max(min(config.starting_volume, 100), 0);
-    let speed = max(min(config.starting_speed, 200), 10);
+    let volume = config.starting_volume.clamp(0, 100);
+    let speed = config.starting_speed.clamp(10, 200);
     let mut runtime_options = RuntimeOptions::new(volume, speed);
     sink.set_speed(runtime_options.speed_decimal);
     sink.set_volume(runtime_options.volume_decimal);
